109f0055b32a61a0a437c36779420cb0d8a5e819
Gustavo Soares <sirgustavo4@gmail.com>
2023-01-16 00:28:41 -0300

    Foi adicionado a interface do projeto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from bank import Bank
 
 class App:
     def __init__(self):
@@ -26,19 +27,26 @@ class App:
         self.window = sg.Window('Cadastro', layout)
     
     def inicio(self):
+       
         while True:
             event, values = self.window.read()
-
+            
             self.nome = values['nome']
             self.sobrenome = values['sobrenome']
             self.turma = values['turma']
             self.data = values['data']
             self.hora = values['hora']
 
+            b1 = Bank(self.nome, self.sobrenome, self.turma, self.data, self.hora)
+
             if event == sg.WINDOW_CLOSED:
+                b1.close_bank()
                 break
-            
+        
             if event == 'Confirmar':
+                
+                b1.config_bank()
+                
                 self.window['output'].update(value=f'{self.nome} {self.sobrenome} da turma {self.turma} foi cadastrado!')
 
 
